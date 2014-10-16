2b74b79fff870233b2f928f7a4dea6436f785c94
lukesmmr <lukas@goodthingseverywhere.com>
2014-10-16 17:18:58 +0200

    Add animated search suggestions in placeholder via typed.js (https://github.com/mattboldt/typed.js/) Include placeholder.js for placeholder cross browser compatibility (JavaScript polyfill for the HTML5 placeholder attribute)

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -10,6 +10,19 @@ $(document).ready(function () {
 			$.cookie('eu-warning', 'warning-accepted', { expires: 7, path: '/' } );
 		});
 	}
+
+	// typed.js animated search suggestions
+	var searchSuggestions = {
+        strings: ["Find Educational Activities", "Black holes", "Galaxies", "Drawing", "Primary School"],
+	    typeSpeed: 10,
+	    backSpeed: -50,
+	    loop: true,
+	    showCursor: false,
+	    backDelay: 3000
+    }
+	$('.home_search_box .search_textbox').attr("placeholder", '');
+	$(".home_search_box .search_textbox").typed(searchSuggestions);
+
 });
 
 /* jQuery Cookie Plugin v1.4.1 */
@@ -138,3 +151,341 @@ $(document).ready(function () {
 	svgeezy.init('nocheck', 'png');
 */
 window.svgeezy=function(){return{init:function(t,i){this.avoid=t||false;this.filetype=i||"png";this.svgSupport=this.supportsSvg();if(!this.svgSupport){this.images=document.getElementsByTagName("img");this.imgL=this.images.length;this.fallbacks()}},fallbacks:function(){while(this.imgL--){if(!this.hasClass(this.images[this.imgL],this.avoid)||!this.avoid){var t=this.images[this.imgL].getAttribute("src");if(t===null){continue}if(this.getFileExt(t)=="svg"){var i=t.replace(".svg","."+this.filetype);this.images[this.imgL].setAttribute("src",i)}}}},getFileExt:function(t){var i=t.split(".").pop();if(i.indexOf("?")!==-1){i=i.split("?")[0]}return i},hasClass:function(t,i){return(" "+t.className+" ").indexOf(" "+i+" ")>-1},supportsSvg:function(){return document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image","1.1")}}}();
+
+
+
+// The MIT License (MIT)
+
+// Typed.js | Copyright (c) 2014 Matt Boldt | www.mattboldt.com
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+
+
+
+!function($){
+
+    "use strict";
+
+    var Typed = function(el, options){
+
+        // chosen element to manipulate text
+        this.el = $(el);
+
+        // options
+        this.options = $.extend({}, $.fn.typed.defaults, options);
+
+        // text content of element
+        this.baseText = this.el.text() || this.el.attr('placeholder') || '';
+
+        // typing speed
+        this.typeSpeed = this.options.typeSpeed;
+
+        // add a delay before typing starts
+        this.startDelay = this.options.startDelay;
+
+        // backspacing speed
+        this.backSpeed = this.options.backSpeed;
+
+        // amount of time to wait before backspacing
+        this.backDelay = this.options.backDelay;
+
+        // input strings of text
+        this.strings = this.options.strings;
+
+        // character number position of current string
+        this.strPos = 0;
+
+        // current array position
+        this.arrayPos = 0;
+
+        // number to stop backspacing on.
+        // default 0, can change depending on how many chars
+        // you want to remove at the time
+        this.stopNum = 0;
+
+        // Looping logic
+        this.loop = this.options.loop;
+        this.loopCount = this.options.loopCount;
+        this.curLoop = 0;
+
+        // for stopping
+        this.stop = false;
+
+        // show cursor
+        this.showCursor = this.isInput ? false : this.options.showCursor;
+
+        // custom cursor
+        this.cursorChar = this.options.cursorChar;
+
+        // attribute to type
+        this.isInput = this.el.is('input');
+        this.attr = this.options.attr || (this.isInput ? 'placeholder' : null);
+
+        // All systems go!
+        this.build();
+    };
+
+        Typed.prototype =  {
+
+            constructor: Typed
+
+            , init: function(){
+                // begin the loop w/ first current string (global self.string)
+                // current string will be passed as an argument each time after this
+                var self = this;
+                self.timeout = setTimeout(function() {
+                    // Start typing
+                    self.typewrite(self.strings[self.arrayPos], self.strPos);
+                }, self.startDelay);
+            }
+
+            , build: function(){
+                // Insert cursor
+                if (this.showCursor === true){
+                  this.cursor = $("<span class=\"typed-cursor\">" + this.cursorChar + "</span>");
+                  this.el.after(this.cursor);
+                }
+                this.init();
+            }
+
+            // pass current string state to each function, types 1 char per call
+            , typewrite: function(curString, curStrPos){
+                // exit when stopped
+                if(this.stop === true)
+                   return;
+
+                // varying values for setTimeout during typing
+                // can't be global since number changes each time loop is executed
+                var humanize = Math.round(Math.random() * (100 - 30)) + this.typeSpeed;
+                var self = this;
+
+                // ------------- optional ------------- //
+                // backpaces a certain string faster
+                // ------------------------------------ //
+                // if (self.arrayPos == 1){
+                //  self.backDelay = 50;
+                // }
+                // else{ self.backDelay = 500; }
+
+                // contain typing function in a timeout humanize'd delay
+                self.timeout = setTimeout(function() {
+                    // check for an escape character before a pause value
+                    // format: \^\d+ .. eg: ^1000 .. should be able to print the ^ too using ^^
+                    // single ^ are removed from string
+                    var charPause = 0;
+                    var substr = curString.substr(curStrPos);
+                    if (substr.charAt(0) === '^') {
+                        var skip = 1;  // skip atleast 1
+                        if(/^\^\d+/.test(substr)) {
+                           substr = /\d+/.exec(substr)[0];
+                           skip += substr.length;
+                           charPause = parseInt(substr);
+                        }
+
+                        // strip out the escape character and pause value so they're not printed
+                        curString = curString.substring(0,curStrPos)+curString.substring(curStrPos+skip);
+                    }
+
+                    // timeout for any pause after a character
+                    self.timeout = setTimeout(function() {
+                        if(curStrPos === curString.length) {
+                           // fires callback function
+                           self.options.onStringTyped(self.arrayPos);
+
+                            // is this the final string
+                           if(self.arrayPos === self.strings.length-1) {
+                              // animation that occurs on the last typed string
+                              self.options.callback();
+
+                              self.curLoop++;
+
+                              // quit if we wont loop back
+                              if(self.loop === false || self.curLoop === self.loopCount)
+                                 return;
+                           }
+
+                           self.timeout = setTimeout(function(){
+                              self.backspace(curString, curStrPos);
+                           }, self.backDelay);
+                        } else {
+
+                           /* call before functions if applicable */
+                           if(curStrPos === 0)
+                              self.options.preStringTyped(self.arrayPos);
+
+                           // start typing each new char into existing string
+                           // curString: arg, self.baseText: original text inside element
+                           var nextString = self.baseText + curString.substr(0, curStrPos+1);
+                           if (self.attr) {
+                            self.el.attr(self.attr, nextString);
+                           } else {
+                            self.el.text(nextString);
+                           }
+
+                           // add characters one by one
+                           curStrPos++;
+                           // loop the function
+                           self.typewrite(curString, curStrPos);
+                        }
+                    // end of character pause
+                    }, charPause);
+
+                // humanized value for typing
+                }, humanize);
+
+            }
+
+            , backspace: function(curString, curStrPos){
+                // exit when stopped
+                if (this.stop === true) {
+                   return;
+                }
+
+                // varying values for setTimeout during typing
+                // can't be global since number changes each time loop is executed
+                var humanize = Math.round(Math.random() * (100 - 30)) + this.backSpeed;
+                var self = this;
+
+                self.timeout = setTimeout(function() {
+
+                    // ----- this part is optional ----- //
+                    // check string array position
+                    // on the first string, only delete one word
+                    // the stopNum actually represents the amount of chars to
+                    // keep in the current string. In my case it's 14.
+                    // if (self.arrayPos == 1){
+                    //  self.stopNum = 14;
+                    // }
+                    //every other time, delete the whole typed string
+                    // else{
+                    //  self.stopNum = 0;
+                    // }
+
+                    // ----- continue important stuff ----- //
+                    // replace text with base text + typed characters
+                    var nextString = self.baseText + curString.substr(0, curStrPos);
+                    if (self.attr) {
+                     self.el.attr(self.attr, nextString);
+                    } else {
+                     self.el.text(nextString);
+                    }
+
+                    // if the number (id of character in current string) is
+                    // less than the stop number, keep going
+                    if (curStrPos > self.stopNum){
+                        // subtract characters one by one
+                        curStrPos--;
+                        // loop the function
+                        self.backspace(curString, curStrPos);
+                    }
+                    // if the stop number has been reached, increase
+                    // array position to next string
+                    else if (curStrPos <= self.stopNum) {
+                        self.arrayPos++;
+
+                        if(self.arrayPos === self.strings.length) {
+                           self.arrayPos = 0;
+                           self.init();
+                        } else
+                            self.typewrite(self.strings[self.arrayPos], curStrPos);
+                    }
+
+                // humanized value for typing
+                }, humanize);
+
+            }
+
+            // Start & Stop currently not working
+
+            // , stop: function() {
+            //     var self = this;
+
+            //     self.stop = true;
+            //     clearInterval(self.timeout);
+            // }
+
+            // , start: function() {
+            //     var self = this;
+            //     if(self.stop === false)
+            //        return;
+
+            //     this.stop = false;
+            //     this.init();
+            // }
+
+            // Reset and rebuild the element
+            , reset: function(){
+                var self = this;
+                clearInterval(self.timeout);
+                var id = this.el.attr('id');
+                this.el.after('<span id="' + id + '"/>')
+                this.el.remove();
+                this.cursor.remove();
+                // Send the callback
+                self.options.resetCallback();
+            }
+
+        };
+
+    $.fn.typed = function (option) {
+        return this.each(function () {
+          var $this = $(this)
+            , data = $this.data('typed')
+            , options = typeof option == 'object' && option;
+          if (!data) $this.data('typed', (data = new Typed(this, options)));
+          if (typeof option == 'string') data[option]();
+        });
+    };
+
+    $.fn.typed.defaults = {
+        strings: ["These are the default values...", "You know what you should do?", "Use your own!", "Have a great day!"],
+        // typing speed
+        typeSpeed: 0,
+        // time before typing starts
+        startDelay: 0,
+        // backspacing speed
+        backSpeed: 0,
+        // time before backspacing
+        backDelay: 500,
+        // loop
+        loop: false,
+        // false = infinite
+        loopCount: false,
+        // show cursor
+        showCursor: true,
+        // character for cursor
+        cursorChar: "|",
+        // attribute to type (null == text)
+        attr: null,
+        // call when done callback function
+        callback: function() {},
+        // starting callback function before each string
+        preStringTyped: function() {},
+        //callback for every typed string
+        onStringTyped: function() {},
+        // callback for reset
+        resetCallback: function() {}
+    };
+
+
+}(window.jQuery);
+
+/* Placeholders.js v3.0.2 */
+(function(t){"use strict";function e(t,e,r){return t.addEventListener?t.addEventListener(e,r,!1):t.attachEvent?t.attachEvent("on"+e,r):void 0}function r(t,e){var r,n;for(r=0,n=t.length;n>r;r++)if(t[r]===e)return!0;return!1}function n(t,e){var r;t.createTextRange?(r=t.createTextRange(),r.move("character",e),r.select()):t.selectionStart&&(t.focus(),t.setSelectionRange(e,e))}function a(t,e){try{return t.type=e,!0}catch(r){return!1}}t.Placeholders={Utils:{addEventListener:e,inArray:r,moveCaret:n,changeType:a}}})(this),function(t){"use strict";function e(){}function r(){try{return document.activeElement}catch(t){}}function n(t,e){var r,n,a=!!e&&t.value!==e,u=t.value===t.getAttribute(V);return(a||u)&&"true"===t.getAttribute(D)?(t.removeAttribute(D),t.value=t.value.replace(t.getAttribute(V),""),t.className=t.className.replace(R,""),n=t.getAttribute(F),parseInt(n,10)>=0&&(t.setAttribute("maxLength",n),t.removeAttribute(F)),r=t.getAttribute(P),r&&(t.type=r),!0):!1}function a(t){var e,r,n=t.getAttribute(V);return""===t.value&&n?(t.setAttribute(D,"true"),t.value=n,t.className+=" "+I,r=t.getAttribute(F),r||(t.setAttribute(F,t.maxLength),t.removeAttribute("maxLength")),e=t.getAttribute(P),e?t.type="text":"password"===t.type&&M.changeType(t,"text")&&t.setAttribute(P,"password"),!0):!1}function u(t,e){var r,n,a,u,i,l,o;if(t&&t.getAttribute(V))e(t);else for(a=t?t.getElementsByTagName("input"):b,u=t?t.getElementsByTagName("textarea"):f,r=a?a.length:0,n=u?u.length:0,o=0,l=r+n;l>o;o++)i=r>o?a[o]:u[o-r],e(i)}function i(t){u(t,n)}function l(t){u(t,a)}function o(t){return function(){m&&t.value===t.getAttribute(V)&&"true"===t.getAttribute(D)?M.moveCaret(t,0):n(t)}}function c(t){return function(){a(t)}}function s(t){return function(e){return A=t.value,"true"===t.getAttribute(D)&&A===t.getAttribute(V)&&M.inArray(C,e.keyCode)?(e.preventDefault&&e.preventDefault(),!1):void 0}}function d(t){return function(){n(t,A),""===t.value&&(t.blur(),M.moveCaret(t,0))}}function g(t){return function(){t===r()&&t.value===t.getAttribute(V)&&"true"===t.getAttribute(D)&&M.moveCaret(t,0)}}function v(t){return function(){i(t)}}function p(t){t.form&&(T=t.form,"string"==typeof T&&(T=document.getElementById(T)),T.getAttribute(U)||(M.addEventListener(T,"submit",v(T)),T.setAttribute(U,"true"))),M.addEventListener(t,"focus",o(t)),M.addEventListener(t,"blur",c(t)),m&&(M.addEventListener(t,"keydown",s(t)),M.addEventListener(t,"keyup",d(t)),M.addEventListener(t,"click",g(t))),t.setAttribute(j,"true"),t.setAttribute(V,x),(m||t!==r())&&a(t)}var b,f,m,h,A,y,E,x,L,T,N,S,w,B=["text","search","url","tel","email","password","number","textarea"],C=[27,33,34,35,36,37,38,39,40,8,46],k="#ccc",I="placeholdersjs",R=RegExp("(?:^|\\s)"+I+"(?!\\S)"),V="data-placeholder-value",D="data-placeholder-active",P="data-placeholder-type",U="data-placeholder-submit",j="data-placeholder-bound",q="data-placeholder-focus",z="data-placeholder-live",F="data-placeholder-maxlength",G=document.createElement("input"),H=document.getElementsByTagName("head")[0],J=document.documentElement,K=t.Placeholders,M=K.Utils;if(K.nativeSupport=void 0!==G.placeholder,!K.nativeSupport){for(b=document.getElementsByTagName("input"),f=document.getElementsByTagName("textarea"),m="false"===J.getAttribute(q),h="false"!==J.getAttribute(z),y=document.createElement("style"),y.type="text/css",E=document.createTextNode("."+I+" { color:"+k+"; }"),y.styleSheet?y.styleSheet.cssText=E.nodeValue:y.appendChild(E),H.insertBefore(y,H.firstChild),w=0,S=b.length+f.length;S>w;w++)N=b.length>w?b[w]:f[w-b.length],x=N.attributes.placeholder,x&&(x=x.nodeValue,x&&M.inArray(B,N.type)&&p(N));L=setInterval(function(){for(w=0,S=b.length+f.length;S>w;w++)N=b.length>w?b[w]:f[w-b.length],x=N.attributes.placeholder,x?(x=x.nodeValue,x&&M.inArray(B,N.type)&&(N.getAttribute(j)||p(N),(x!==N.getAttribute(V)||"password"===N.type&&!N.getAttribute(P))&&("password"===N.type&&!N.getAttribute(P)&&M.changeType(N,"text")&&N.setAttribute(P,"password"),N.value===N.getAttribute(V)&&(N.value=x),N.setAttribute(V,x)))):N.getAttribute(D)&&(n(N),N.removeAttribute(V));h||clearInterval(L)},100)}M.addEventListener(t,"beforeunload",function(){K.disable()}),K.disable=K.nativeSupport?e:i,K.enable=K.nativeSupport?e:l}(this);
