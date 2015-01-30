f84d22006a07c110824cd8cc4f63839493ca82d2
Bruno Rino <bruno.rino@gmail.com>
2015-01-30 23:36:37 +0000

    tests for UUIDField

--- astroedu/django_ext/tests.py
+++ astroedu/django_ext/tests.py
@@ -1,6 +1,32 @@
 from django.test import TestCase
 
+from .models import UUIDField
+
 
 class ArchivalModelTests(TestCase):
     def test_featured(self):
         pass
+
+
+class UUIDFieldTests(TestCase):
+
+    def test_blank_true(self):
+        my_field_instance = UUIDField(blank=True)
+        name, path, args, kwargs = my_field_instance.deconstruct()
+        new_instance = UUIDField(*args, **kwargs)
+        self.assertEqual(my_field_instance.max_length, new_instance.max_length)
+        self.assertEqual(my_field_instance.blank, new_instance.blank)
+
+    def test_blank_false(self):
+        my_field_instance = UUIDField(blank=False)
+        name, path, args, kwargs = my_field_instance.deconstruct()
+        new_instance = UUIDField(*args, **kwargs)
+        self.assertEqual(my_field_instance.max_length, new_instance.max_length)
+        self.assertEqual(my_field_instance.blank, new_instance.blank)
+
+    def test_max_length(self):
+        my_field_instance = UUIDField(max_length=77)
+        name, path, args, kwargs = my_field_instance.deconstruct()
+        new_instance = UUIDField(*args, **kwargs)
+        self.assertEqual(my_field_instance.max_length, new_instance.max_length)
+        self.assertEqual(my_field_instance.blank, new_instance.blank)
