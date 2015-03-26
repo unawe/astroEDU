d1b71ee32c39e7554543b2fb352d0444b8a1b8d7
Bruno Rino <bruno.rino@gmail.com>
2015-03-26 15:30:11 +0100

    fixed bug when AdminModels would not define a 'fieldsets_and_inlines_order'

--- astroedu/templatetags/admin_extras.py
+++ astroedu/templatetags/admin_extras.py
@@ -16,12 +16,16 @@ def get_fieldsets_and_inlines(context):
     inlines = iter(context['inline_admin_formsets'])
 
     fieldsets_and_inlines = []
-    for choice in model_admin.fieldsets_and_inlines_order:
-        if choice == 'f':
-            fieldsets_and_inlines.append(('f', adminform.next()))
-        elif choice == 'i':
-            fieldsets_and_inlines.append(('i', inlines.next()))
 
+    # add groups of fieldsets and inlines according to 'fieldsets_and_inlines_order'
+    if hasattr(adminform, 'fieldsets_and_inlines_order'):
+        for choice in model_admin.fieldsets_and_inlines_order:
+            if choice == 'f':
+                fieldsets_and_inlines.append(('f', adminform.next()))
+            elif choice == 'i':
+                fieldsets_and_inlines.append(('i', inlines.next()))
+
+    # add remaining fieldsets and inlines as usual
     for fieldset in adminform:
         fieldsets_and_inlines.append(('f', fieldset))
     for inline in inlines:
