f3a6d016c322d883e13d1e20b067ded250347575
Bruno Rino <bruno.rino@gmail.com>
2017-03-06 12:05:44 +0100

    code moved to django-apps

--- astroedu/activities/urls_collections.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from astroedu.activities import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.collections_list, name='list'),
-    url(r'^(?P<collection_slug>[a-zA-Z0-9-]+)/$', views.collections_detail, name='detail'), 
-)
