500b06308e96dad03e88d64420c1e3f6e909886b
Bruno Rino <bruno.rino@gmail.com>
2017-03-11 16:25:01 +0100

    removed celery dependency

--- astroedu/__init__.py
+++
@@ -1,8 +0,0 @@
-# these environment variables must be defined before celery
-import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'astroedu.settings')
-os.environ.setdefault('DJANGO_SETTINGS_CONFIG', 'PROD')
-
-# This will make sure the celery app is always imported when
-# Django starts so that shared_task will use this app.
-from .celery import app as celery_app
