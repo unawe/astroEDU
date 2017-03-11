327da12f3635a0c1d0a4fbcdc3b8bb5e00cac940
Bruno Rino <bruno.rino@gmail.com>
2017-03-11 17:07:27 +0100

    added DJANGO_SETTINGS_CONFIG

--- astroedu/wsgi.py
+++ astroedu/wsgi.py
@@ -23,6 +23,9 @@ from django.core.wsgi import get_wsgi_application
 # os.environ["DJANGO_SETTINGS_MODULE"] = "astroedu.settings"
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'astroedu.settings')
 
+# use production settings
+os.environ.setdefault('DJANGO_SETTINGS_CONFIG', 'PROD')
+
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
